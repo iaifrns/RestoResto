21eb715e4884eee335ea8e62d9530698ca67b309
iaifrns <nsinifranc@gmail.com>
2025-04-29 12:56:05 +0100

    changes in this section

--- src/sections/HeaderSection.tsx
+++ src/sections/HeaderSection.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Dottes } from "../components/DecorationBox";
+import { Parallax } from "react-scroll-parallax";
 
 interface HeaderType {
   title: string;
@@ -9,15 +10,17 @@ interface HeaderType {
 
 const HeaderSection: React.FC<HeaderType> = ({ title, text, image }) => {
   return (
-    <div className="w-full h-[500px] relative">
-      <div className="absolute h-full w-full flex flex-col items-center justify-end bg-[rgba(29,29,29,0.7)] pb-30">
+    <div className="w-full h-[500px] relative overflow-hidden">
+      <div className="absolute h-full w-full flex flex-col items-center justify-end bg-[rgba(29,29,29,0.7)] pb-30 z-10">
         <p className="font-playfair font-semibold text-secondary text-xl">
           {title}
         </p>
         <p className="font-bold font-playfair text-[48px] text-white mb-4">{text}</p>
         <Dottes />
       </div>
+      <Parallax speed={-30} className="z-0 w-full ">
       <img src={image} alt={image} className="w-full h-full object-cover" />
+      </Parallax>
     </div>
   );
 };
